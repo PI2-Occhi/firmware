606d39bd79850468e06015b874bcf2ef5560ba46
matheusgvieira <matheusmaxi9.0@gmail.com>
2022-03-18 00:55:19 -0300

    feat(camocchi): create class for capture images

---
+++ build/lib/camocchi/test.py
@@ -0,0 +1,18 @@
+from time import sleep
+from picamera import PiCamera
+
+
+# Create object picamera
+camera = PiCamera()
+
+# Define resolution
+camera.resolution = (1024, 768)
+
+# Connected to an instance of PiPrevewRenderer
+camera.start_preview()
+
+# Wait for 2 seconds
+sleep(2)
+
+# Capture photo
+camera.capture("example.png")
