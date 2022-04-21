3ccc3a63dbd37d44e37f69635198163982baeb3b
Matheus Gois Vieira <matheusmaxi9.0@gmail.com>
2022-04-21 19:40:52 +0100

    Finished capture image

--- camocchi/continuos_simple.py
+++ camocchi/continuos_simple.py
@@ -1,4 +1,6 @@
 import cv2
+import sys
+import os
 
 # Create object VideoCapture
 cam = cv2.VideoCapture(0)
@@ -7,11 +9,14 @@ while True:
     # initialize the camera and get photo
     ret, image = cam.read()
 
+    file_name_image = os.getenv("OCCHI_PATH_IMAGE", "image.png")
+
     # Use the cvtColor() function to grayscale the image
     img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
     # Equalize image
-    # equ = cv2.equalizeHist(img)
+    if (len(sys.argv) > 1 and sys.argv[1]):
+        img = cv2.equalizeHist(img)
 
     # Store image
-    cv2.imwrite("image.png", img)
+    cv2.imwrite(file_name_image, img)
